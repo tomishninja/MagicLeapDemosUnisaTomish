6b47cf30f81ef1dce51555f9d861189986ab413e
Clarke <clatj010@mymail.unisa.edu.au>
2019-06-03 17:27:54 +0930

    minor changes getting ready for larger changes when I import the new 3D ready version of the system

---
+++ Assets/Scripts/PNMIntArrayObject1.cs
@@ -0,0 +1,30 @@
+﻿namespace PNMtoBuffer
+{
+    public class PNMIntArrayObject
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Width { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Height { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Breath { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Scale { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int[] Pixels { get; set; }
+    }
+}
